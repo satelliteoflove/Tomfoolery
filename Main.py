ff9b989c39566dd68a07945296f6ccdaf4e5b065
satelliteoflove <clchildress@gmail.com>
2015-07-05 19:22:47 -0400

    Move to working with Pyglet Windows

--- Main.py
+++ Main.py
@@ -1,39 +1,35 @@
 __author__ = 'Chris'
 
-import random
-
-statusAilments = {}
-
-races = {"Elf": (6,8), "Dwarf": (10,8), "Human": (8,8)}
-'The "races" dictionary contains each race, with the following stats pre-defined:' \
-'   strength' \
-'   vitality' \
-'example: dict = {"Key1": (ValX1, ValY1, ValZ1), "Key2": (ValX2, ValY2, ValZ2),...,"Key99": (ValX99, ValY99, ValY99)}'
+import random, pyglet
 
 class Character(object):
     'Common base class for all PCs and NPCs.'
     def __init__(self):
         self.name = input("What is the player's name?\n")
+        self.strength = 8
+        self.vitality = 8
         self.hitPoints = random.randint(5,30)
         self.attack = 10
         self.bonusPoints = random.randint(5,25)
-        self.strength = 8
-        self.vitality = 8
         self.defense = 1
 
 
-def MakeCharacter():
-    playerchar = Character()
-    print("Name: %s     Points To Spend: %s" % (playerchar.name, playerchar.bonusPoints))
-    print("Strength: %s   Vitality: %s" % (playerchar.strength, playerchar.vitality))
-    choice1 = input("Spend points on (S)trength, or (V)itality?")
-    while playerchar.bonusPoints > 0:
-        if choice1 == "S":
-            choice2 = input("How many points?")
-            if choice2 <= playerchar.bonusPoints and choice2 > 0:
-                playerchar.strength += choice2
-                playerchar.bonusPoints -= choice2
-            #elif choice2 < 0:
+#Create a Pyglet Window with default values.
+window = pyglet.window.Window()
 
-print(races['Elf'])
-MakeCharacter()
+#Create a "Label", which apparently is just rendered text on the screen.
+label = pyglet.text.Label('This is my first text.  I imagine it will wrap at least once,'
+                          ' as this is a large amount of text for just a single line.',
+                          font_name='Times New Roman',
+                          font_size=36, x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center',multiline=True, width=500)
+@window.event()
+def onKeyPress(symbol, modifiers):
+    print('A key was pressed.')
+
+@window.event()
+def on_draw():
+    window.clear()
+    label.draw()
+
+pyglet.app.run()
