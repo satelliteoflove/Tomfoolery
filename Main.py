9b07c6a11e73c05e6bd7aadeb8e31597971185cc
satelliteoflove <clchildress@gmail.com>
2015-08-02 22:53:47 -0700

    Completely reworked design to use dictionaries within dictionaries for room layout, and will use it for race-based specs, soon.

--- Main.py
+++ Main.py
@@ -1,6 +1,10 @@
 __author__ = 'Chris'
 
-import random, pyglet
+import random
+
+races = {
+    "elf" : {"strength":}
+}
 
 class Character(object):
     'Common base class for all PCs and NPCs.'
@@ -8,45 +12,80 @@ class Character(object):
         self.name = input("What is the player's name?\n")
         self.strength = 8
         self.vitality = 8
-        self.hitPoints = random.randint(5,30)
+        self.hitPoints = (self.vitality / 10) * random.randint(5,30)
         self.attack = 10
         self.bonusPoints = random.randint(5,25)
         self.defense = 1
+        self.inventory = []
+
+    def showStats(self):
+        print(self.name + " Statistics:")
+        print("STR: " + str(self.strength))
+        print("VIT: " + str(self.vitality))
+        print("HP: " + str(self.hitPoints))
+        print("ATK: " + str(self.attack))
+        print("DEF: " + str(self.defense))
+
+def showInstructions():
+    '''print a (temporary) "main menu", and the available commands'''
+    print("WizardryClone")
+    print("=============")
+    print("Commands:")
+    print("'go [direction]'")
+    print("'get [item]'")
 
 
-#Create a Pyglet Window with default values.
-main_window = pyglet.window.Window(caption='Main Window')
-message_window = pyglet.window.Window(caption='Message Window')
+def showStatus(character):
+    '''print the player's current status'''
+    print("===================")
+    print("You are in the " + rooms[currentRoom]["name"])
+    character.showStats()
+    print("Inventory: " + str(character.inventory))
+    #list items, none if empty
+    if "item" in rooms[currentRoom]:
+        print("In the room, you see a " + rooms[currentRoom]["item"])
 
-#label = pyglet.text.Label('This is my first text.  I imagine it will wrap at least once,'
-#                          ' as this is a large amount of text for just a single line.',
-#                          font_name='Times New Roman',
-#                          font_size=36, x=main_window.width//2, y=main_window.height//2,
-#                          anchor_x='center', anchor_y='center',multiline=True, width=500)
+'''A dictionary representation of a single dungeon level'''
+rooms = {
+    1: { "name":"Hall",
+         "east":2,
+         "south":3},
+    2: { "name":"Bedroom",
+         "west":1,
+         "south":4,
+         "item":"sword"},
+    3: { "name":"Kitchen",
+         "north":1},
+    4: { "name":"Bathroom",
+         "north":2}
+         }
 
-scrolling_text = pyglet.text.layout.ScrollableTextLayout('This is some text to scroll.  It is my hope, '
-                                                         'that the pyglet functionality built in to the '
-                                                         'ScrollableTextLayout thingy will not require '
-                                                         'the specific use of the newline character.  But,'
-                                                         ' it probably will require that and much more.  '
-                                                         'Hard to say.  This however is a running paragraph that '
-                                                         'might as well be Latin.',500,500)
-message_label = pyglet.text.Label('This is for the message window.  This message will also'
-                                  ' automatically wrap due to length, but probably in a '
-                                  'different place.', font_name='Arial', font_size=18,
-                                  x=message_window.width//2, y=message_window.height//2,
-                                  anchor_x='center', anchor_y='center', multiline=True, width=500)
+#create character
+player = Character()
 
-@message_window.event()
-def on_draw():
-    message_window.clear()
-    message_label.draw()
+#start the player off in a room
+currentRoom = 1
 
-@main_window.event()
-def on_draw():
+#display "main menu"
+showInstructions()
 
-    main_window.clear()
-    #label.draw()
-    scrolling_text.draw()
+#loop
+while True:
 
-pyglet.app.run()
+    showStatus(player)
+
+    move = input(">").lower().split()
+
+    if move[0] == "go":
+        if move[1] in rooms[currentRoom]:
+            currentRoom = rooms[currentRoom][move[1]]
+        else:
+            print("You can't go that way.")
+
+    if move[0] == "get":
+        if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]["item"]:
+            player.inventory += [move[1]]
+            print(move[1] + " taken.")
+            del rooms[currentRoom]["item"]
+        else:
+            print("You can't take " + move[1] + ".")
