4d00b221af26d4524bb379955f9c498b815825aa
satelliteoflove <clchildress@gmail.com>
2016-11-06 22:49:10 -0500

    Adding functionality for NPCs

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 __author__ = 'Chris'
 
 import random
-import math
-import numpy
 
-
-#Globals
+# Globals
 world_characters = []
 MAP_WIDTH = 0
 MAP_HEIGHT = 0
+
 
 class Item(object):
     def __init__(self):
@@ -20,9 +18,11 @@ class Item(object):
         self.is_equipped = False
         self.can_be_equipped = False
 
+
 class Weapon(Item):
     """All weapons can be described with this class."""
-    def __init__(self,name,min_dmg,max_dmg,description):
+
+    def __init__(self, name, min_dmg, max_dmg, description):
         self.isweapon = True
         self.name = name
         self.min_dmg = min_dmg
@@ -31,25 +31,30 @@ class Weapon(Item):
         self.is_equipped = True
         self.description = description
 
+
 class Character(object):
     """Common base class for all PCs and NPCs."""
+
     def __init__(self):
         self.name = input("What is the player's name?\n")
         self.strength = 8
         self.vitality = 8
-        self.hitPoints = int((self.vitality / 10) * random.randint(5,30))
+        self.hitPoints = int((self.vitality / 10) * random.randint(5, 30))
         self.attack = 1
-        self.bonusPoints = random.randint(5,25)
+        self.bonusPoints = random.randint(5, 25)
         self.defense = 1
         self.inventory = []
-        self.currentRoom = (0,0)
+        self.currentRoom = (0, 0)
         self.add_to_room()
+        self.direction = "north"
         world_characters.append(self)
         self.equipment = []
+
     def add_to_room(self):
         for chamber in rooms:
             if chamber == self.currentRoom:
                 rooms[self.currentRoom]["characters"].append(self)
+
     def show_stats(self):
         """Displays current character stats."""
         print("Statistics for " + self.name)
@@ -58,6 +63,7 @@ class Character(object):
         print("HP: " + str(self.hitPoints))
         print("ATK: " + str(self.attack))
         print("DEF: " + str(self.defense))
+
     def show_inventory(self):
         if len(self.inventory) > 0:
             for item in self.inventory:
@@ -67,13 +73,16 @@ class Character(object):
                     print(item.name)
         else:
             print("You aren't carrying anything.")
-        #list items, none if empty
+            # list items, none if empty
+
     def place_random(self):
-        #start the player off in a room
+        # start the player off in a room
         self.currentRoom = random.randrange()
-    def place_room(self, room:int):
+
+    def place_room(self, room: int):
         if room > 0 and room <= len(rooms):
             self.currentRoom = room
+
     def view_surroundings(self):
         """print what the player can see"""
         currentRoom = self.currentRoom
@@ -99,7 +108,8 @@ class Character(object):
             print("East")
         if "west" in rooms[currentRoom]:
             print("West")
-    def go(self,newroom):
+
+    def go(self, newroom):
         if move[1] in rooms[self.currentRoom]:
             rooms[self.currentRoom]["characters"].remove(self)
             self.currentRoom = rooms[self.currentRoom][move[1]]
@@ -107,7 +117,8 @@ class Character(object):
             self.view_surroundings()
         else:
             print("You can't go that way.")
-    def get(self,item_to_get):
+
+    def get(self, item_to_get):
         temp_item_list = []
         for item in rooms[self.currentRoom]["items"]:
             temp_item_list.append(item.name)
@@ -119,7 +130,8 @@ class Character(object):
                     rooms[self.currentRoom]["items"].remove(item)
         else:
             print("You can't take " + item_to_get + ".")
-    def drop(self,item_to_drop):
+
+    def drop(self, item_to_drop):
         temp_item_list = []
         for item in self.inventory:
             temp_item_list.append(item.name)
@@ -132,7 +144,7 @@ class Character(object):
         else:
             print("Drop what, again?")
 
-    def equip(self,item_to_equip):
+    def equip(self, item_to_equip):
         temp_item_list = []
         for item in self.inventory:
             temp_item_list.append(item.name)
@@ -140,22 +152,42 @@ class Character(object):
             for item in self.inventory:
                 if item_to_equip == item.name and item.can_be_equipped is True:
                     item.is_equipped = True
-                    print(item.name + " equipped."
+                    print(item.name + " equipped.")
 
-class Enemy(Character):
-    def __init__(self):
-        self.name = input("What is enemy called?\n")
+
+# TODO: Create "go" method which uses the current direction and moves char/npc there.
+class NonPlayerCharacter(Character):
+    """Common base class for all NPCs."""
+
+    def __init__(self, name):
+        self.name = name
         self.strength = 3
         self.vitality = 3
-        self.hitPoints = int((self.vitality / 10) * random.randint(5,30))
+        self.hitPoints = int((self.vitality / 10) * random.randint(5, 30))
         self.attack = 1
-        self.bonusPoints = random.randint(5,25)
         self.defense = 1
         self.inventory = []
-        self.currentRoom = (place_random)
+        self.currentRoom = (0, 0)
         self.add_to_room()
         world_characters.append(self)
         self.equipment = []
+        self.movement = 1
+        self.direction = "north"
+
+    def change_direction(self):
+        directions = ["north", "south", "east", "west"]
+        directions.remove(self.direction)
+        self.direction = random.choice(directions)
+
+    # def go(self, newroom):
+    #     if move[1] in rooms[self.currentRoom]:
+    #         rooms[self.currentRoom]["characters"].remove(self)
+    #         self.currentRoom = rooms[self.currentRoom][move[1]]
+    #         rooms[self.currentRoom]["characters"].append(self)
+    #         self.view_surroundings()
+    #     else:
+    #         print("You can't go that way.")
+
 
 def showInstructions():
     """print a (temporary) "main menu", and the available commands"""
@@ -170,56 +202,57 @@ def showInstructions():
     print("'look'")
     print("'attack'")
 
-def party_add_character(character_type,n):
+
+def party_add_character(character_type, n):
     """Creates a list of Character objects"""
 
-#Item initialization - item names are case sensitive for now
-sword1 = Weapon("sword",1,5,"This is a simple short sword of steel.")
+
+# Item initialization - item names are case sensitive for now
+sword1 = Weapon("sword", 1, 5, "This is a simple short sword of steel.")
 item1 = Item()
 
-
-
-#A single "dungeon" level
+# A single "dungeon" level
 rooms = {
-    (0,0): { "name":"0,0",
-         "description":"0,0",
-         "east":(1,0),
-         "north":(0,1),
-         "items":[],
-         "characters":[]},
-    (0,1): { "name":"0,1",
-         "description": "0,1",
-         "south":(0,0),
-         "east":(1,1),
-         "items":[sword1,item1],
-         "characters":[]},
-    (1,0): { "name":"1,0",
-         "description":"1,0",
-         "north":(1,1),
-         "west":(0,0),
-         "items":[],
-         "characters":[]},
-    (1,1): { "name":"1,1",
-         "description":"1,1",
-         "south":(1,0),
-         "west":(0,1),
-         "items":[],
-         "characters":[]}
-         }
-
-#A list of weapons
-weapons = {
-    "sword01":{"name":"Short Sword"}
+    (0, 0): {"name": "0,0",
+             "description": "0,0",
+             "east": (1, 0),
+             "north": (0, 1),
+             "items": [],
+             "characters": []},
+    (0, 1): {"name": "0,1",
+             "description": "0,1",
+             "south": (0, 0),
+             "east": (1, 1),
+             "items": [sword1, item1],
+             "characters": []},
+    (1, 0): {"name": "1,0",
+             "description": "1,0",
+             "north": (1, 1),
+             "west": (0, 0),
+             "items": [],
+             "characters": []},
+    (1, 1): {"name": "1,1",
+             "description": "1,1",
+             "south": (1, 0),
+             "west": (0, 1),
+             "items": [],
+             "characters": []}
 }
 
-#create characters, list of characters
-player = Character()
-player2 = Character()
+# A list of weapons
+weapons = {
+    "sword01": {"name": "Short Sword"}
+}
 
-#display "main menu"
+# create characters, list of characters
+player = Character()
+goblin = NonPlayerCharacter("goblin")
+orc = NonPlayerCharacter("orc")
+
+# display "main menu"
 showInstructions()
 
-#loop
+# loop
 while True:
 
     move = input(">").lower().split()
@@ -254,3 +287,10 @@ while True:
 
     if move[0] == "q" or move[0] == "quit":
         break
+
+    for character in world_characters:
+        if isinstance(character, NonPlayerCharacter):
+            print(character.name + " changes direction.")
+            character.change_direction()
+            print(character.name + " is now pointing %s" %character.direction)
+            character.go(character.direction)
