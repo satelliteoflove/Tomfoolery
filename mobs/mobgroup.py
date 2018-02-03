b1e0576d166122d20ec5ca626852ada7ee3e6303
autodoc <autodoc@ludditelabs.io>
2018-02-03 04:34:57 +0000

    Update API documentation with autodoc
    6:5: [python:D001] missing docstring: __init__
    12:5: [python:D001] missing docstring: add_to_level
    16:5: [python:D001] missing docstring: list_members

--- mobs/mobgroup.py
+++ mobs/mobgroup.py
@@ -4,15 +4,29 @@ from numpy import random
 class MobGroup(object):
     """Class for storing monsters. For use during combat."""
     def __init__(self, mob_list, max_weight):
+        """
+
+        Args:
+            mob_list:
+            max_weight:
+        """
         self.members = []
         self.remaining_weight = max_weight
         self.mob_list = mob_list
         self.members.append(mobs.Mob(self.mob_list, "goblin", 1))
 
     def add_to_level(self, level):
+        """
+
+        Args:
+            level:
+        """
         for member in self.members:
             level.mob_party.append(self)
 
     def list_members(self):
+        """
+
+        """
         for member in self.members:
             print(member.name)
