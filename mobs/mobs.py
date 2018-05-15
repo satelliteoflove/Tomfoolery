dee7a1ad289258daa7c34bf217e8bebd113d0c37
= <=>
2018-05-15 03:46:10 +0000

    just so it's not lost...

--- mobs/mobs.py
+++ mobs/mobs.py
@@ -6,12 +6,14 @@ import items
 class Mob(object):
     def __init__(self, mob_list, type_name, level):
         self.type_name = mob_list[type_name]["typename"]
+        self.type = mob_list[type_name]["type"]
         self.name = self.type_name
         self.level = level
+        self.agility = mob_list[type_name]["agility"]
         self.HP = mob_list[type_name]["base_hp"] * self.level
         self.THAC0 = mob_list[type_name]["THAC0"]
         self.AP = mob_list[type_name]["base_ap"] * self.level // 2
-        self.weight = mob_list[type_name]["party_weight"] * self.level
+        self.weight = round(mob_list[type_name]["party_weight"] * self.level, 1)
         self.weapon_slots = mob_list[type_name]["wslots"]
         self.attacks = mob_list[type_name]["attacks"]
         self.alignment = mob_list[type_name]["alignment"]
