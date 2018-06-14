1841dcb198a0cafbf36c9f37e2b53e1cc67d82bf
= <=>
2018-06-14 02:49:09 +0000

    Merged weapon into items.

--- actors/items/items.py
+++ actors/items/items.py
@@ -1,3 +1,5 @@
+import effects
+
 class Item(object):
     """ Root item class.
     """
@@ -10,11 +12,21 @@ class Item(object):
         self.weight = itemtype["weight"]
         self.equippable = itemtype["equippable"]
         self.break_chance = itemtype["break_chance"]
-        #TODO: implement item repair system
-        #self.reparable = itemtype["reparable"]
 
     def bind_affix(self, effect):
         self.affix_effect = effect
 
     def bind_suffix(self, effect):
         self.suffix_effect = effect
+
+class Weapon(Item):
+    """All weapons can be described with this class."""
+    def __init__(self, list_of_weapons, weapon_index):
+        self.isweapon = True
+        self.name = list_of_weapons[weapon_index]["name"]
+        self.min_dmg = list_of_weapons[weapon_index]["min_dmg"]
+        self.max_dmg = list_of_weapons[weapon_index]["max_dmg"]
+        self.can_be_equipped = True
+        self.is_equipped = False
+        self.description = list_of_weapons[weapon_index]["description"]
+
