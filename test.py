9cbcf0271e3567ad8f3902c8d0619f92bc9a281f
Chris Childress <clchildress@gmail.com>
2018-07-06 08:51:42 -0400

    2018-07-06 08:51

---
+++ test.py
@@ -0,0 +1,11 @@
+def menu():
+    print("Dungeon! \n 1. Create PC \n 2. Encounter Mobs")
+    choice = input()
+
+    if choice == "1":
+        print("Creating a PC...")
+
+    if choice == "2":
+        print("Monsters incoming!")
+
+menu()
