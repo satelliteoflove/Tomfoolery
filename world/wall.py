e8bd205b8a77d25eb3ed499443d7f163f86b1041
autodoc <autodoc@ludditelabs.io>
2018-07-12 20:41:36 +0000

    Update API documentation with autodoc
    8:5: [python:D001] missing docstring: __init__

--- world/wall.py
+++ world/wall.py
@@ -6,6 +6,11 @@ class Wall(object):
     Keyword arguments:
     walltype -- The type name for the wall (str)."""
     def __init__(self, walltype):
+        """
+
+        Args:
+            walltype:
+        """
         self.name = walltype["name"]
         self.visible = walltype["visible"]
         self.passable = walltype["passable"]
