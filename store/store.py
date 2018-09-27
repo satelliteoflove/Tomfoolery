b16eff089a786e6afbb308a1e0fe33436449f3e0
Chris Childress <clchildress@gmail.com>
2018-09-27 16:52:20 -0400

    Removing "impure" functionality from Store module.

--- store/store.py
+++ store/store.py
@@ -1,21 +1,17 @@
 class Store(object):
+    """Buy, sell, identify and remove cursed items."""
     def __init__(self):
+        """Standard Store object for feeding items."""
         self.inventory = {}
-        self.current_player = None
-        self.current_party = None
-
-    def pool_gold(self):
-       self.current_party.pool_gold(current_player) 
 
     def buy_item(self, item):
         self.inventory[item.uuid] = item
         return item.sell_value
 
-    def sell_item(self, item, char_money):
-        funds = char_money
+    def sell_item(self, item, available_money):
+        funds = available_money
         if funds >= item.sell_price:
             self.inventory[item.uuid].__delitem__()
             return item.sell_price
-        else
-            print("Not enough funds.")
-            return 0
+        else:
+            return False
